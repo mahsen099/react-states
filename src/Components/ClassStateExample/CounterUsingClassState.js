136036874933eff15a6764c73243ccf378c2b01f
mahsen099 <75514973+mahsen099@users.noreply.github.com>
2021-02-09 21:18:52 -0600

    c

--- src/Components/ClassStateExample/CounterUsingClassState.js
+++ src/Components/ClassStateExample/CounterUsingClassState.js
@@ -1,26 +1,45 @@
-import React, { Component } from "react";
+import React, { useState } from 'react'
 
-class CounterUsingClassState extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      count: 0,
-    };
+function CounterUsingClassState() {
+  const [count, setcount]= useState(0);
+
+  function abebe(){
+    setcount((count)=>count+1)
   }
 
-  incrementCount = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.incrementCount}>Count: {this.state.count}</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+     {/* <button onClick={abebe}>Count: {count}</button>  */}
+     {/* method2 */}
+     <button onClick={()=>{setcount((count)=>count+1)}}>Count: {count}</button>
+    </div>
+  )
 }
-
 export default CounterUsingClassState;
+
+// import React, { Component } from "react";
+
+// class CounterUsingClassState extends Component {
+//   constructor(props) {
+//     super();
+//     this.state = {
+//       count: 0,
+//     };
+//   }
+
+//   incrementCount = () => {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.incrementCount}>Count: {this.state.count}</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default CounterUsingClassState;
--- src/Components/ClassStateExample/UsingClassLifeCycleComponent.js
+++ src/Components/ClassStateExample/UsingClassLifeCycleComponent.js
@@ -1,38 +1,19 @@
-import React, { Component } from "react";
+import React, {useState, useEffect} from 'react';
 
-class UsingClassLifeCycleComponent extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      count: 0,
-    };
-  }
+function UsingClassLifeCycleComponent() {
 
-  componentDidMount() {
-    console.log("componentDidMount");
-    document.title = `Count: ( ${this.state.count} )`;
-  }
+  const [count, setCount]= useState(0);
+  useEffect(()=>{
+    document.title= `count:${count}`
+  },[count]);
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate");
-    // We can use conditional statements in here to run the title update only if the count value has changed
-    // Helpful when there are multiple update functionalities that are implemented
-    // console.log(prevState.count);
-    if (prevState.count != this.state.count) {
-      document.title = `Count: ( ${this.state.count} )`;
-    }
-  }
-
-  render() {
-    console.log("Render");
-    return (
-      <div>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Count {this.state.count}
+  return (
+    <div>
+      <button onClick={() =>setCount((count)=>count+1) }>
+          Count {count}
         </button>
-      </div>
-    );
-  }
+    </div>
+  )
 }
+export default UsingClassLifeCycleComponent
 
-export default UsingClassLifeCycleComponent;
